79f2b407a7a6059da6e31cddc80437eface85396
Sara <wlot31@gmail.com>
2025-02-20 10:49:18 +1100

    Initial Commit

---
+++ Test1/Test1App.swift
@@ -0,0 +1,17 @@
+//
+//  Test1App.swift
+//  Test1
+//
+//  Created by Sara Al Fahdawi on 20/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct Test1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
