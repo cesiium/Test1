462194ca892ad40fc91236659ab14f0cdfbc4a76
Sara <wlot31@gmail.com>
2025-02-20 11:30:12 +1100

    Created a slider and dog years calculator

--- Test1/ContentView.swift
+++ Test1/ContentView.swift
@@ -8,12 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+    @State private var age = 0.0
+    
+    func calculateDogYears() -> Double {
+        return age * 7
+    }
+     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+            switch age{
+            case 0...15:
+                Text("🐶")
+                    .font(.system(size: 100))
+            case 0...25:
+                Text("💀")
+                    .font(.system(size: 100))
+            default:
+                Text("❌")
+                    .font(.system(size: 100))
+                
+            }
+           
+            Text("Dog Years Calculator")
+                .font(.largeTitle)
+                .fontDesign(.rounded)
+                .foregroundStyle(.brown)
+                .fontWeight(.black)
+            Slider(value: $age, in: 0...25, step:1)
+            Text("My Age: \(Int(age))")
+                .font(.title2)
+                .fontWeight(.bold)
+            Text("Age in dog years: \(Int(calculateDogYears()))")
+                .font(.title2)
+                .fontWeight(.bold)
+    
+           
         }
         .padding()
     }
